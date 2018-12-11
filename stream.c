e60a55cf8b2a639e30a31d20649de2660cf13a9c
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2018-12-11 14:39:40 -0700

    Add distribute

--- stream.c
+++ stream.c
@@ -23,7 +23,7 @@ void triad( const int N, const DATA_T scalar) {
     int i;
 
 
-#pragma omp target teams
+#pragma omp target teams distribute
     for ( i = 0; i < N; i++ ) {
         a[i] = b[i] * scalar + c[i];
     }
